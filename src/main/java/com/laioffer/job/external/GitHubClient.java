c90ea9624f2f470e0d3b6a40c3355c6302e911eb
Weirdguy790 <veralively7239@gmail.com>
2022-02-16 20:55:39 -0500

    Finish building up GitHub API, and send outgoing request. Just use search method and keyword to search job positions.

--- src/main/java/com/laioffer/job/external/GitHubClient.java
+++ src/main/java/com/laioffer/job/external/GitHubClient.java
@@ -1,21 +1,26 @@
 package com.laioffer.job.external;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.job.entity.Item;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Array;
 import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class GitHubClient {
     private static final String URL_TEMPLATE = "https://jobs.github.com/positions.json?description=%s&lat=%s&long=%s";  //%s : String 占位符
     private static final String DEFAULT_KEYWORD = "engineer";
 
-    public String search(double lat, double lon, String keyword) {
+    public List<Item> search(double lat, double lon, String keyword) {
         if (keyword == null) {
             keyword = DEFAULT_KEYWORD;
         }
@@ -32,15 +37,18 @@ public class GitHubClient {
         CloseableHttpClient httpclient = HttpClients.createDefault();
 
         // Create a custom response handler
-        ResponseHandler<String> responseHandler = response -> {
+        ResponseHandler<List<Item>> responseHandler = response -> {
             if (response.getStatusLine().getStatusCode() != 200) {
-                return ""; //no results
+                return Collections.emptyList(); //no results
             }
             HttpEntity entity = response.getEntity();
             if (entity == null) {
-                return ""; //no results
+                return Collections.emptyList(); //no results
             }
-            return EntityUtils.toString(entity); //entity type : application/json
+            //return EntityUtils.toString(entity); //entity type : application/json
+            ObjectMapper mapper = new ObjectMapper();
+            Item[] itemArray = mapper.readValue(entity.getContent(), Item[].class); //.class :
+            return Arrays.asList(itemArray);
         };
 
         try {
@@ -48,6 +56,6 @@ public class GitHubClient {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return "";
+        return Collections.emptyList();
     }
 }
--- src/main/java/com/laioffer/job/servlet/SearchServlet.java
+++ src/main/java/com/laioffer/job/servlet/SearchServlet.java
@@ -1,11 +1,16 @@
 package com.laioffer.job.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.job.entity.Item;
+import com.laioffer.job.external.GitHubClient;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "SearchServlet", urlPatterns = {"/search"})
 public class SearchServlet extends HttpServlet {
@@ -14,6 +19,15 @@ public class SearchServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().write("This is SearchServlet");
+        double lat = Double.parseDouble(request.getParameter("lat"));
+        double lon = Double.parseDouble(request.getParameter("lon"));
+
+        GitHubClient client = new GitHubClient();
+        List<Item> items = client.search(lat, lon, null);
+        //we will add more stuff here
+        ObjectMapper mapper = new ObjectMapper();
+        response.setContentType("application/json");
+        mapper.writeValue(response.getWriter(), items);
+
     }
 }
